a846621910d6c669561b8809c1892cc3dfd8c1c7
HNgoc <144669382+hngoc-29@users.noreply.github.com>
2026-07-17 06:10:44 +0700

    Add import for ValueListenable in viewer_screen.dart

--- lib/presentation/screens/viewer/viewer_screen.dart
+++ lib/presentation/screens/viewer/viewer_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart' show ValueListenable;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:collection/collection.dart';
